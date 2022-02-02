b9a5f3c2809a8dd1289b88a4bf68e1426d8f0bea
dbrentan <danielbrentan@gmail.com>
2022-02-02 16:59:55 -0300

    Primeiras aulas de CSharp na Alura

---
+++ AprendendoCSharp/CriandoVariaveis/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CriandoVariaveis
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 2 - Criando Variaveis");
+
+            int idade;
+            idade = 32;
+            Console.WriteLine("Lucas tem " + idade + " anos");
+
+            idade = 10 + 5;
+            Console.WriteLine($"Julia tem {idade} anos");
+
+            idade = 10 + 5 * 2;
+            Console.WriteLine($"Daniel tem {idade} anos");
+
+            idade = (10 - 5) * 2;
+            Console.WriteLine("Amanda tem " + idade + " anos");
+
+        }
+    }
+}
